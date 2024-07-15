f2e934541a23c8b582e3bb904f14963cd957e417
dhananjaykuber <dnkuber2002@gmail.com>
2024-07-15 19:22:29 +0530

    setup wordpress and git

---
+++ plugins/college-gateway/college-gateway.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: College Gateway
+ * Description: All backend functionality will take place in this plugin. Like, registering post type, taxonomy, widget and meta box.
+ * Plugin URI:  https://github.com/dhananjaykuber/college-gateway
+ * Author:      Dhananjay Kuber
+ * Author URI:  https://github.com/dhananjaykuber/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Version:     1.0
+ * Text Domain: college-gateway
+ *
+ * @package College_Gateway
+ */
