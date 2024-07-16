52a9286d0d8ac35e3b9696ddaf31cdf1f82820ed
dhananjaykuber <dnkuber2002@gmail.com>
2024-07-16 18:47:04 +0530

    add metaboxes bootstrap class

---
+++ plugins/college-gateway/inc/classes/class-meta-boxes.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Load all classes that register meta box.
+ *
+ * @package College_Gateway
+ */
+
+namespace College_Gateway\Inc;
+
+use College_Gateway\Inc\Traits\Singleton;
+use College_Gateway\Inc\Meta_Boxes\Metabox_Teacher_General_Information;
+use College_Gateway\Inc\Meta_Boxes\Metabox_Teacher_Social_Information;
+
+/**
+ * Class Meta_Boxes
+ */
+class Meta_Boxes {
+
+	use Singleton;
+
+	/**
+	 * Construct method.
+	 */
+	protected function __construct() {
+		// Load all meta boxes classes.
+		Metabox_Teacher_General_Information::get_instance();
+		Metabox_Teacher_Social_Information::get_instance();
+	}
+}
